34b27213dd5602a138112f67ac8a61dab95cba4d
Conor Thompson <conorthompson213@gmail.com>
2019-04-04 18:19:55 +0100

    Added rooms

--- chat_room_client.go
+++ chat_room_client.go
@@ -1,29 +1,43 @@
 package main
 
-import "net"
-import "fmt"
-import "bufio"
-import "os"
+import( 
+ "net"
+ "fmt"
+ "bufio"
+ "os"
+ "time"
+ 
+ )
+
+const (
+  TIME_FORMAT = "3:04PM"
+  ENTER_ALIAS = "Enter alias here: "
+  ENTER_MESSAGE = "Enter in a message below:\n"
+  CONN_TYPE = "tcp"
+  CONN_HOST = "localhost"
+  CONN_PORT = "80"
+)
+
+var name string 
+var room string
 
 func main() {
 
   // connect to this socket
-  conn, _ := net.Dial("tcp", "localhost:80")
-  fmt.Print("Enter alias here: ")
-  var name string 
-  fmt.Scanln(&name)
-  name = "["+name+"]: "
+  conn, _ := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+  login(conn)
+  room = "["+room+"]"
   
-  go listen(conn)
-
   for { 
     // read in input from stdin
     reader := bufio.NewReader(os.Stdin)
-    fmt.Print(name)
     text, _ := reader.ReadString('\n')
+    time_sent := "["+time.Now().Format(TIME_FORMAT)+"]"
+    
     // send to socket
-    fmt.Fprintf(conn, name+text + "\n")
-    // listen for reply
+    if text != "" {
+    sendMessage(conn, room+time_sent+name+text + "\n")
+  }
     
   }
 }
@@ -31,6 +45,81 @@ func main() {
 func listen(conn net.Conn){
   for{
     message, _ := bufio.NewReader(conn).ReadString('\n')
-    fmt.Println("\n"+message)
+    fmt.Println("\n"+string(message))
   }
+}
+
+func login(conn net.Conn){
+  fmt.Print(ENTER_ALIAS) 
+  fmt.Scanln(&name)
+  name = "["+name+"]: "
+
+  fmt.Print("Create or Join a room: ")
+  fmt.Scanln(&room)
+  joinRoom(conn,room)
+  
+  go listen(conn) 
+
+  fmt.Print(ENTER_MESSAGE)
+}
+
+func sendMessage(conn net.Conn, message string){
+  fmt.Fprintf(conn, message)
+}
+
+func joinRoom(conn net.Conn, room string) {
+  request := "[JOIN_ROOM]" + room
+  sendMessage(conn,request)
+}
+// func joinRoom(conn net.Conn, room string) bool{
+//   request := "[JOIN_ROOM]" + room
+//   var message string 
+
+//   sendMessage(conn,request)
+//   go func(conn net.Conn) {
+//     for{
+//       message, _ = bufio.NewReader(conn).ReadString('\n')
+//       log(message)
+//       if message == "success" {
+//         log("You have succesfully joined room: "+room)
+//         break
+//       }
+//     }
+//     }(conn)
+
+//     if message == "success" {
+//       return true
+//     }
+//     log("dang")
+//     return false
+// }
+
+// func createRoom(conn net.Conn, room string) bool{
+//   request := "[CREATE_ROOM]" + room 
+//   //sendMessage(conn,request)
+//   var status string
+//   fmt.Fprintf(conn, request)
+//   //go func(conn net.Conn) {
+//     for{
+//       message, _, err := bufio.NewReader(conn).ReadString('\n')
+//       if err !=nil{
+//         log(err.Error())
+//       }
+//       if message == "success" {
+//         log("You have succesfully joined room: "+room)
+//         status = message
+//         break
+//       }
+//     }
+//     //}(conn)
+
+//     if status == "success" {
+//       return true
+//     }
+//     log("dang")
+//     return false
+// }
+
+func log(message string){
+  fmt.Println(message+"\n")
 }
--- chat_room_server.go
+++ chat_room_server.go
@@ -4,33 +4,42 @@ import (
     "fmt"
     "net"
     "os"
+    "time"
+    "regexp"
+    "strings"
     )
 
 const (
     CONN_HOST = "localhost"
     CONN_PORT = "80"
     CONN_TYPE = "tcp"
+    ERROR = "Error"
+    INFO = "Info"
+    SUCCESS = "success"
 )
+
 var connections []net.Conn
+var rooms = make(map[string][]net.Conn)
 
 func main() {
     // Listen for incoming connections.
     l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
     
     if err != nil {
-        fmt.Println("Error listening:", err.Error())
+        log(err.Error(),ERROR)
         os.Exit(1)
     }
     // Close the listener when the application closes.
     defer l.Close()
-    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+    log("Listening on " + CONN_HOST + ":" + CONN_PORT,INFO)
+
     for {
         // Listen for an incoming connection.
         conn, err := l.Accept()
         connections = append(connections,conn)
 
         if err != nil {
-            fmt.Println("Error accepting: ", err.Error())
+            log(err.Error(),ERROR)
             os.Exit(1)
         }
 
@@ -42,10 +51,17 @@ func main() {
 
 // Handles incoming requests.
 func broadcast(message string, conn net.Conn) {
-  fmt.Printf("%d",len(connections))
-  for _, connection := range connections{
+  
+  r, _ := regexp.Compile("^(.*?)]")
+  match := r.FindString(message)
+  match = match[1:]
+  match = match[:len(match)-1]
+
+  for k, v := range rooms { 
+    fmt.Printf("key[%s] value[%s]\n", k, v)
+}
+  for _, connection := range rooms[match]{
     if conn != connection {
-    fmt.Println("------------------")
     connection.Write([]byte(message))
   }
 }
@@ -58,16 +74,68 @@ func handleRequest(conn net.Conn){
   for{
   _, err := conn.Read(buf)
   message := string(buf)
+
   if err != nil {
-    fmt.Println("Error reading:", err.Error())
+    log(err.Error(),ERROR)
     break
   }
-  // Send a response back to person contacting us.
-  fmt.Print(message)
 
-  // broadcast the message
-  go broadcast(message,conn)
+  log(message,INFO)
+  if message[:11] == "[JOIN_ROOM]"{ 
+    //TODO: grab only characters and not whitespace from room
+    r, _ := regexp.Compile("(\\S)*")
+    match := r.FindString(message)
+    log("["+match+"]",INFO)
+    if joinRoom(conn,message[11:]){
+     sendMessage(conn,SUCCESS)
+    }else if createRoom(conn,message[11:]) && joinRoom(conn,message[11:]){
+      sendMessage(conn,SUCCESS)
+    }
+  } else{
+    go broadcast(message,conn)
+}
   // Close the connection when you're done with it.
   }
   conn.Close()
+}
+
+func createRoom(conn net.Conn, room string) bool{
+  fmt.Printf("["+room+"]")
+  if _, ok := rooms[room]; ok {
+      sendMessage(conn,"failed")
+      return false
+  } else{
+    log("room created:"+ room,INFO)
+    rooms[room] = append(rooms[room],conn)
+    return true
+  }
+}
+
+func joinRoom(conn net.Conn, room string) bool{
+  room = strings.TrimSpace(room[:len(room)-1])
+  if _, ok := rooms[room]; ok {
+    rooms[room] = append(rooms[room],conn)
+    log("Joined room "+room,INFO)
+    return true
+  } else{
+    sendMessage(conn, "failed")
+    return false
+  }
+}
+
+func sendMessage(conn net.Conn, message string){
+  conn.Write([]byte(message+"\n"))
+  log("sent message "+message,INFO)
+}
+
+func log(message string, type_ string){
+      log_time := "["+time.Now().Format("3:04PM")+"]"
+      ERROR := "[ERROR]"
+      INFO := "[INFO]"
+
+      if type_ != "Error" {
+        fmt.Println(INFO+log_time+": "+message)
+      }else{
+        fmt.Println(ERROR+log_time+": "+message)
+      }
 }
